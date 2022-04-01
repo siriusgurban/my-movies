1d4883229ad35d4929bda8917ad69d8a8d089c14
Your Name <you@example.com>
2022-04-01 14:48:09 +0400

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1>SearchBar</h1>
+            
+
+            </div>
+            
+        )
+    }
+}
+
+export default SearchBar;
